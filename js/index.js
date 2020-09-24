87b56ab8ba6a20280d6e4cf28b797d4e33f6c794
Natali Baziuchenko <baziuchenko_natali@ukr.net>
2020-09-24 22:55:44 +0300

    Репозиторий для JS основ

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// Задание 1
+// const name = "«Генератор защитного поля»"; // Write code on this line
+// let price = 1000; // Write code on this line
+// const firstMessage = "Выбран «Генератор защитного поля», цена за штуку 1000 кредитов"; // Write code on this line
+// price = 2000;
+// const secondMessage = "Выбран «Генератор защитного поля», цена за штуку 1000 кредитов"; // Write code on this line
+
+// console.log(firstMessage);
+
+// console.log(secondMessage);
+
+// Задание 2
+// const invoice = 100; 
+// const stock = 100;
+
+// // Write code under this line
+// const message = invoice <= 100 ? 'Заказ оформлен, с вами свяжется менеджер' : 'На складе недостаточно товаров!' ;
+
+// console.log(message);
+
+// Задание 3
+const userPassword = 'jqueryismyjam';
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
+
+if (userPassword === ADMIN_PASSWORD) {
+console.log("message"); 
+}
