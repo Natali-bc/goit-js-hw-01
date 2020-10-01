fc4f44c1ff6c34bcc2994639fb68df6fb0c3de9b
Natali Baziuchenko <baziuchenko_natali@ukr.net>
2020-10-01 09:58:42 +0300

    Добавление 2го модуля

--- js/module2.js
+++ js/module2.js
@@ -65,16 +65,132 @@
 
 // Задача 5
 
-function checkForSpam (str) { 
-  'use strict';
-  // Write code under this line
+// function checkForSpam (str) { 
   
-}
+// return(str.toLowerCase().includes("spam")||str.toLowerCase().includes("sale"))
 
-//console.log(checkForSpam('Latest technology news')); // false
+// }
 
-//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// // console.log(checkForSpam('Latest technology news')); // false
 
-//console.log(checkForSpam('Get best sale offers now!')); // true
+// // console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// // console.log(checkForSpam('Get bestsale offers now!')); // true
+
+// // console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+// Задача 6
+
+// function mapArray(array) {
+//   'use strict';
+//   const numbers = new Array(array.length);
+//   for(let i = 0; i < array.length; i += 1) {
+  
+    
+//   }
+//   return numbers;
+// }
+
+// console.log(mapArray([-2, 0, 2]));
+// [-20, 0, 20]
+
+//console.log(mapArray([-2.5, 0, 2.5]));
+// [-25, 0, 25]
+
+// Задача 7
+
+// function filterArray(array) {
+//   'use strict';
+//   const numbers = [];
+//   for(let i = 0; i < array.length; i += 1) {
+//     if (Number.isFinite(array[i])) {
+//       numbers.push(array[i])}
+    
+//   }
+//   return numbers;
+// }
+
+// console.log(filterArray([-2, 0, 2]));
+// [-2, 0, 2]
+
+// console.log(filterArray([1, NaN, Infinity]));
+// [1]
+
+// console.log(filterArray([0, -0, 100, '100']));
+// [0, 0, 100]
+
+// console.log(filterArray([undefined, false, null, [], 1]));
+// [1]
+
+// console.log(filterArray([{}, () => {}, 2]));
+// [2]
+
+// Задача 8
+
+// function reduceArray(array) {
+//   'use strict';
+//   let total = 0;
+//  for (let i = 0; i < array.length; i += 1) {
+//   total += array[i];
+//  }
+   
+//   return total;
+// };
+
+// console.log(reduceArray([1, 2, 3]));
+// 6
+
+// console.log(reduceArray([-2, 0, 2]));
+// 0
+
+// console.log(reduceArray([1, 2, 2.5]));
+// 5.5
+
+// Задача 9
+
+// function isLoginValid (login, min = 4, max = 16) {
+//   return (login.length >=min && login.length <= max)
+    
+// }
+
+// function isLoginUnique  (allLogins, login) {
+//   'use strict';
+//   return (!allLogins.includes(login));
+ 
+// }
+
+// function addLogin (allLogins, login) {
+//   'use strict';
+//   const SUCCESS = 'Логин успешно добавлен!';
+//   const REFUSAL = 'Такой логин уже используется!';
+//   const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
+//   let message;
+ 
+// if (isLoginValid(login) === false) {
+//   message = ERROR;
+// }
+//  else if (isLoginUnique(allLogins, login) === false) {
+//   message = REFUSAL;
+// } 
+// else {
+//   allLogins.push(login);
+//   message = SUCCESS;
+// }
+//  return message;
+
+// }
+
+
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+// console.log(addLogin(logins, 'Ajax')); 
+// 'Логин успешно добавлен!'
+
+// console.log(addLogin(logins, 'robotGoogles')); 
+// 'Такой логин уже используется!'
+
+// console.log(addLogin(logins, 'Zod'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); 
+// 'Ошибка! Логин должен быть от 4 до 16 символов' 
